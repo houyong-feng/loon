f47aff2c5e2b7249506e65cd0cb0b44b3f25eda8
chinnkarahoi <chinnkarahoi@gmail.com>
2021-12-03 15:53:34 +0800

    update

--- gua_opencard76.js
+++ gua_opencard76.js
@@ -33,7 +33,7 @@ All变量适用
 
 ================Loon==============
 [Script]
-cron "40 1,10 1-15 12 *" script-path=gua_opencard76.js
+cron "40 1,10,17 1-15 12 *" script-path=gua_opencard76.js
 
 ============Quantumultx===============
 [task_local]
